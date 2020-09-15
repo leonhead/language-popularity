f7f65f63d2623f9c26e9fa1f078c2c036f1a89bc
qubit <qubit07@gmail.com>
2020-09-15 19:36:05 +0200

    add created field to database script

--- src/main/resources/sql/create_database.sql
+++ src/main/resources/sql/create_database.sql
@@ -6,17 +6,19 @@ DROP TABLE if exists program_language;
 
 CREATE TABLE program_language(
     program_language_id int NOT NULL AUTO_INCREMENT,
-    program_language_name varchar(40) UNIQUE,
+    program_language_name varchar(40) NOT NULL UNIQUE,
     
     PRIMARY KEY (program_language_id)
    );
 
 CREATE TABLE github_repository(
     github_repository_id int NOT NULL AUTO_INCREMENT,
-	full_name varchar(40),
-    html_url varchar(40),
+    github_id int NOT NULL UNIQUE,
+	full_name varchar(255) NOT NULL UNIQUE,
+    html_url varchar(255),
 	stargazers_count int,
     program_language_id int,
+    created long,
     FOREIGN KEY (program_language_id) REFERENCES program_language(program_language_id),
 
     PRIMARY KEY (github_repository_id)
