0fa3942b0c1eef826c5c14b45adab549b9d522ba
qubit <qubit07@gmail.com>
2020-09-17 17:24:38 +0200

    popularity table

--- src/main/resources/sql/create_database.sql
+++ src/main/resources/sql/create_database.sql
@@ -2,7 +2,9 @@ CREATE DATABASE if not exists program_languages;
 USE program_languages;
 
 DROP TABLE if exists github_repository;
+DROP TABLE if exists popularity;
 DROP TABLE if exists program_language;
+
 
 CREATE TABLE program_language(
     program_language_id int NOT NULL AUTO_INCREMENT,
@@ -10,6 +12,16 @@ CREATE TABLE program_language(
     
     PRIMARY KEY (program_language_id)
    );
+
+CREATE TABLE popularity(
+	popularity_id int NOT NULL AUTO_INCREMENT,
+    date_year int,
+    currency double,
+    program_language_id int,
+    FOREIGN KEY (program_language_id) REFERENCES program_language(program_language_id),
+    
+	PRIMARY KEY (popularity_id)
+);
 
 CREATE TABLE github_repository(
     github_repository_id int NOT NULL AUTO_INCREMENT,
--- src/main/resources/sql/insert_data.sql
+++ src/main/resources/sql/insert_data.sql
@@ -1,5 +1,17 @@
 USE program_languages;
 
+INSERT INTO popularity (date_year, currency)
+VALUES (2020, 5.0);
+
+INSERT INTO popularity (date_year, currency)
+VALUES (2020, 2.0);
+
+INSERT INTO popularity (date_year, currency)
+VALUES (2020, 0.0);
+
+INSERT INTO popularity (date_year, currency)
+VALUES (2020, 2.7);
+
 INSERT INTO program_language (program_language_name)
 VALUES ('Java');
 
@@ -12,12 +24,12 @@ VALUES ('C++');
 INSERT INTO program_language (program_language_name)
 VALUES ('Python');
 
-INSERT INTO github_repository (full_name, html_url, stargazers_count, program_language_id)
-VALUES ('vault/tresos', 'http://github.com/vault/tresos', 200, 1);
+INSERT INTO github_repository (github_id, full_name, html_url, stargazers_count, program_language_id)
+VALUES (1, 'vault/tresos', 'http://github.com/vault/tresos', 200, 1);
 
-INSERT INTO github_repository (full_name, html_url, stargazers_count, program_language_id)
-VALUES ('unicorn/sun', 'http://github.com/unicorn/sun', 169, 3);
+INSERT INTO github_repository (github_id, full_name, html_url, stargazers_count, program_language_id)
+VALUES (2, 'unicorn/sun', 'http://github.com/unicorn/sun', 169, 3);
 
-INSERT INTO github_repository (full_name, html_url, stargazers_count, program_language_id)
-VALUES ('prometheus/angle', 'http://github.com/prometheus/angle', 279, 3);
+INSERT INTO github_repository (github_id, full_name, html_url, stargazers_count, program_language_id)
+VALUES (3, 'prometheus/angle', 'http://github.com/prometheus/angle', 279, 3);
 
